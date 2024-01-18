34bfc4461ccebcf16437b8b7474708aab97ae98a
murilosntdev <murilosntdev@gmail.com>
2024-01-18 15:20:32 -0300

    DB config

---
+++ src/database/db.js
@@ -0,0 +1,26 @@
+import pg from "pg";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new pg.Pool({
+    user: process.env.POSTGRE_USER,
+    host: process.env.POSTGRE_HOST,
+    database: process.env.POSTGRE_DATABASE,
+    password: process.env.POSTGRE_PASSWORD,
+    port: process.env.POSTGRE_PORT
+});
+
+export const dbExecute = (query, params = []) => {
+    return new Promise((response) => {
+        pool.query(query, params, (error, result) => {
+            if (error) {
+                const errorContent = {};
+                errorContent.dbError = error;
+                response(errorContent);
+            } else {
+                response(result);
+            }
+        });
+    });
+};
