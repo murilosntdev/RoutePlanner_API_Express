a4f66d788182a2e2a18c4eeb1e8e49c7b45ee05d
murilosntdev <murilosntdev@gmail.com>
2024-01-19 17:24:41 -0300

    bug: duplicate company error not working

--- src/models/Account.js
+++ src/models/Account.js
@@ -4,13 +4,7 @@ export const selectIdByCnpjEmail = async (cnpj, email) => {
     var query = `SELECT id FROM company WHERE (cnpj = $1) OR (email = $2)`;
     var result = await dbExecute(query, [cnpj, email]);
 
-    if (result.dbError) {
-        return (result);
-    } else if (!result.rows[0]) {
-        return ({ "rows": [] });
-    }
-
-    return (result.rows[0]);
+    return (result);
 }
 
 export const insertAllCompany = async (name, cnpj, email, hashPassword, status) => {
