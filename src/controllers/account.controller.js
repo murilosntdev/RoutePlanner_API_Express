ab30ac8278f5d219e72088af3639c12015dbe8ad
murilosntdev <murilosntdev@gmail.com>
2024-01-19 17:13:59 -0300

    Route newCompany

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,2 +1,34 @@
-export const newCompany = (req, res) => {
+import { insertAllCompany } from "../models/Account.js";
+import { errorResponse } from "../services/responses.js/error.response.js";
+import { successResponse } from "../services/responses.js/success.response.js";
+import * as bcrypt from "bcrypt";
+
+export const newCompany = async (req, res) => {
+    const name = req.body.name;
+    const cnpj = req.body.cnpj;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const hashPassword = bcrypt.hashSync(password, 10);
+
+    const insertInfos = await insertAllCompany(name, cnpj, email, hashPassword, "NEW_ACCOUNT");
+
+    if (insertInfos.dbError) {
+        res.status(503);
+        res.json(errorResponse(503, null, insertInfos));
+        return;
+    }
+
+    const responseDetail = {
+        "result": `Companhia '${insertInfos.name}' criada com sucesso`,
+        "account_info": {
+            "type": "company",
+            "cnpj": insertInfos.cnpj,
+            "status": insertInfos.status
+        }
+    };
+
+    res.status(201);
+    res.json(successResponse(201, responseDetail));
+    return;
 }
--- src/models/Account.js
+++ src/models/Account.js
@@ -7,8 +7,19 @@ export const selectIdByCnpjEmail = async (cnpj, email) => {
     if (result.dbError) {
         return (result);
     } else if (!result.rows[0]) {
-        return { "rows": [] };
+        return ({ "rows": [] });
     }
 
-    return result.rows[0];
+    return (result.rows[0]);
+}
+
+export const insertAllCompany = async (name, cnpj, email, hashPassword, status) => {
+    var query = `INSERT INTO company (name, cnpj, email, password, status) VALUES ($1, $2, $3, $4, $5) RETURNING name, cnpj, status`;
+    var result = await dbExecute(query, [name, cnpj, email, hashPassword, status]);
+
+    if (result.dbError) {
+        return (result);
+    };
+
+    return (result.rows[0]);
 }
